5d122e264dc9e794e12afa029c44b9737f92647d
Parag Jhunjhunwala <87004753+paragjjw@users.noreply.github.com>
2023-10-28 17:03:46 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -140,14 +140,3 @@ def predict_img():
             return render_template("index.html")
     else:
         return render_template("index.html")
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Flask app exposing yolov5 models")
-    parser.add_argument("--port", default=5000, type=int, help="port number")
-    args = parser.parse_args()
-    # model = torch.hub.load('.', 'custom','best_246.pt', source='local')
-    # model.eval()
-    # debug=True causes Restarting with stat
-    app.run(host="0.0.0.0", port=args.port, debug=False)
